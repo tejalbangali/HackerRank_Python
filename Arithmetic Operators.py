ed8cc3f1703a657496bddaa6559c6d15402b97ea
Tejal Bangali <63381263+tejalbangali@users.noreply.github.com>
2021-05-13 21:33:41 +0530

    Arithmetic Operations

---
+++ Arithmetic Operators.py
@@ -0,0 +1,17 @@
+# --> Write a Program to print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference between the two numbers (first - second).
+# The third line contains the product of the two numbers.
+ 
+#     Sample Input:       Sample Output: 
+#        3                              5
+#        2                              1
+#                                       6
+
+
+a=int(input())
+b=int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
